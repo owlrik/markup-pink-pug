04a52c3615bd445b58accbe6ffa2766d0c9e34c1
Vadim Tiuriumin <owlriks@gmail.com>
2021-10-23 12:14:22 +0800

    Исправлено отображение меню при отключенном JavaScript

--- source/js/utils/triggerClickHandler.js
+++ source/js/utils/triggerClickHandler.js
@@ -4,9 +4,6 @@
   let navigationMenu = document.querySelector('.main-nav');
   let navigationMenuBurger = navigationMenu.querySelector('.main-nav__trigger');
 
-  // TODO: вынести из этого скрипта в секцию head
-  navigationMenu.classList.remove('main-nav--no-js');
-
   let onTriggerClick = function(evt) {
     evt.preventDefault();
     navigationMenu.classList.toggle('main-nav--closed');
